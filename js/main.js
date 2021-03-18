49d0738bdd9a9a71522a3f286121a64ddde393e7
qusayeug <evgen.kusey@gmail.com>
2021-03-18 18:40:15 +0300

    fix: stupid vscode fixxxxx

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.hotel-slider', {
+const hotelSlider = new Swiper('.hotel-slider', {
     // Optional parameters
     loop: true,
   
@@ -11,4 +11,21 @@ const swiper = new Swiper('.hotel-slider', {
         enabled: true,
         onlyInViewport: false,
       },
+  });
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    }
+  });
+
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    console.log('Клик по кнопке меню');
+    document.querySelector(".navbar-bottom")
+    .classList.toggle('navbar-bottom--visible');
   });
