8057f4adfd194cfed8317bc89e822420927678e6
qusayeug <evgen.kusey@gmail.com>
2021-03-18 18:37:05 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const hotelSlider = new Swiper('.hotel-slider', {
+const swiper = new Swiper('.hotel-slider', {
     // Optional parameters
     loop: true,
   
@@ -11,14 +11,4 @@ const hotelSlider = new Swiper('.hotel-slider', {
         enabled: true,
         onlyInViewport: false,
       },
-  });
-  const reviewsSlider = new Swiper('.reviews-slider', {
-    // Optional parameters
-    loop: true,
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.reviews-slider__button--next',
-      prevEl: '.reviews-slider__button--prev',
-    }
   });
