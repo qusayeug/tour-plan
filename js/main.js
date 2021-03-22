c01b83dd163f53806e05cc5ac2da315458988025
qusayeug <evgen.kusey@gmail.com>
2021-03-22 22:17:15 +0300

    fix: media for grid

--- js/main.js
+++ js/main.js
@@ -51,39 +51,23 @@ $(document).ready(function () {
   }
 
   // Обработка форм
-  $('.modal__form').each(function(){
+  $('.form').each(function() {
     $(this).validate({
       errorClass: "invalid",
       messages: {
         name: {
           required: "Please specify your name",
-          minlength: "Name must be at least 2 letters long",
+          minlength: "At least 2 letters"
         },
         email: {
-          required: "We need your email address to contact you",
+          required: "We need your email address",
           email: "Your email address must be in the format of name@domain.com"
         },
         phone: {
-          required: "Phone is required",
+          required: "Telephone required",
         },
-      },
+      }
     });
-  })
-  $('.footer__form').validate({
-    errorClass: "invalid",
-    messages: {
-      name: {
-        required: "Please specify your name",
-        minlength: "Name must be at least 2 letters long",
-      },
-      email: {
-        required: "We need your email address to contact you",
-        email: "Your email address must be in the format of name@domain.com"
-      },
-      phone: {
-        required: "Phone is required",
-      },
-    },
   });
   $('.phone').mask('+7 (999) 999-99-99');
 });
